d28276a6fd7ae49aed9cedaeb852ba7f2c2a3078
MajPaji <sababi@kth.se>
2021-02-23 13:22:41 +0000

    producs and fixtures

---
+++ products/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendy_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, blank=True, null=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
+    image_url = models.URLField(max_length=1024, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+    
